b7bee8377e7cb7f9ae6a15ced51be21d258687a5
Ed <alpezed@gmail.com>
2026-02-24 21:41:46 +0800

    Add Montserrat font family and update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useEffect } from 'react';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+	anchor: '(tabs)',
 };
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+SplashScreen.preventAutoHideAsync();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+	const colorScheme = useColorScheme();
+	const [loaded, error] = useFonts({
+		Montserrat: require('@/assets/fonts/Montserrat-Regular.ttf'),
+		'Montserrat-SemiBold': require('@/assets/fonts/Montserrat-SemiBold.ttf'),
+		'Montserrat-Bold': require('@/assets/fonts/Montserrat-Bold.ttf'),
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
+	return (
+		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+			<Stack>
+				<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+				<Stack.Screen
+					name='modal'
+					options={{
+						presentation: 'modal',
+						title: 'Modal',
+					}}
+				/>
+			</Stack>
+			<StatusBar style='auto' />
+		</ThemeProvider>
+	);
 }
